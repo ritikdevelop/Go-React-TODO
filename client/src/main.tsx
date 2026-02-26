3d1294a8846b3612c4ed161bd2453cd824fd78e4
ritikdevelop <hrithikrana216@gmail.com>
2026-02-26 15:15:57 +0530

    style: format JSX indentation in main.tsx for improved readability

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -10,11 +9,11 @@ import theme from "./chakra/theme.ts";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<ChakraProvider theme={theme}>
-				<App />
-			</ChakraProvider>
-		</QueryClientProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
